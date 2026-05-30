96bcc9d37fa2e5fa99e1ff382b23dd27772b2d4a
aswathi-48 <aswathik482@gmail.com>
2026-05-30 14:20:48 +0530

    route set to app.js

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ import express from 'express';
 const app = express()
 import dotenv from 'dotenv'
 import connection from './config/db.js';
+import userRoutes from './routes/userRoutes.js'
+import cors from 'cors'
 dotenv.config()
 
 const PORT = process.env.PORT
 
 connection()
+app.use(cors())
+
+app.use('/user',userRoutes)
 
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`);
