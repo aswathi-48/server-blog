a5762e6e1bb3cd4fa4600d0ddaee60e39c5a0deb
aswathi-48 <aswathik482@gmail.com>
2026-05-30 15:34:01 +0530

    blog chnages

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createBlog, deleteBlog, getBlogById, getBlogs, updateBlog } from "../controllers/blogController.js";
+import { auth } from "../middleware/auth.js";
 
 const router = Router()
 
-router.post("/create", createBlog);
+router.post("/create", auth, createBlog);
 router.get("/", getBlogs);
 router.get("/:id", getBlogById);
-router.put("/:id", updateBlog);
-router.delete("/:id", deleteBlog);
+router.put("/:id", auth, updateBlog);
+router.delete("/:id", auth, deleteBlog);
 
 export default router
