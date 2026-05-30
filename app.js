cfff098fd402468ecfc6be0bcdf599872be38a53
aswathi-48 <aswathik482@gmail.com>
2026-05-30 10:47:01 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const app = express()
 import dotenv from 'dotenv'
+import connection from './config/db.js';
 dotenv.config()
 
 const PORT = process.env.PORT
 
+connection()
 
 
 
